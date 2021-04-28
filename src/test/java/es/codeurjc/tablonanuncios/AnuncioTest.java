94242ce0d754278149edb6dda0ce1d6456d8b405
Maes95 <michel.maes95@gmail.com>
2021-04-28 12:35:02 +0200

    Add headless option

--- src/test/java/es/codeurjc/tablonanuncios/AnuncioTest.java
+++ src/test/java/es/codeurjc/tablonanuncios/AnuncioTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
@@ -31,7 +32,11 @@ public class AnuncioTest {
 	
 	@BeforeEach
 	public void setup() {
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
 	}
 	
 	@AfterEach
